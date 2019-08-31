88c929b0f5de72606a58ad2be7629eba9168e500
thecurrentuser <thecurrentuser@live.com>
2019-08-31 13:39:08 -0300

    Criado controller para o cadastro de pessoas, formPessoas

---
+++ seguirapp/src/main/java/com/seguirapp/controller/PessoaController.java
@@ -0,0 +1,14 @@
+package com.seguirapp.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class PessoaController {
+
+	@RequestMapping("/cadastroPessoa")
+	public String form() {
+		return "pessoa/formPessoa";
+	}
+	
+}
